5052bd84427c4b038678cf4858033060e860379e
Ansh-123 <anshsthakkar2008@gmail.com>
2020-12-19 22:19:00 -0800

    Add exercise class

---
+++ app/src/main/java/com/rishav/gloginexample/structures/Exercise.java
@@ -0,0 +1,53 @@
+package com.rishav.gloginexample.structures;
+
+public class Exercise {
+
+    private String name;
+    private String description;
+    private double amount;
+    private String animation;
+    private String unitOfMeasure;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public String getAnimation() {
+        return animation;
+    }
+
+    public void setAnimation(String animation) {
+        this.animation = animation;
+    }
+
+    public String getUnitOfMeasure() {
+        return unitOfMeasure;
+    }
+
+    public void setUnitOfMeasure(String unitOfMeasure) {
+        this.unitOfMeasure = unitOfMeasure;
+    }
+
+
+
+}
